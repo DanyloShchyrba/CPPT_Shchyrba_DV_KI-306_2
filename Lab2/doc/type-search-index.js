15048c4ab66cc6041fdacd208a6765944ee9ba10
DanyloShchyrba <shchyrbadanylo@gmail.com>
2023-10-03 13:04:03 +0300

    Edited javadoc for lab2

--- Lab2/doc/type-search-index.js
+++ Lab2/doc/type-search-index.js
@@ -1,1 +1,1 @@
-typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"KI306.Shchyrba.Lab2","l":"Lightbulb"}];updateSearchResults();
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"KI306.Shchyrba.Lab2","l":"Lightbulb"},{"p":"KI306.Shchyrba.Lab2","l":"LightbulbApp"}];updateSearchResults();
